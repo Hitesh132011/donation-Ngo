28ff1646d16895b5c9539b071f53144702b30cad
Hitesh Mohanty <hiteshmohanty33@gmail.com>
2024-06-05 12:51:55 +0530

    second commit

--- backend/controllers/checkout.js
+++ backend/controllers/checkout.js
@@ -2,3 +2,11 @@ import axios from "axios";
 import crypto from "crypto";
 import { Volunteer } from "../models/volunteer";
 const cryptomous=axios.create({baseURL: ""})
+const createInvoice=async(amount)=>{
+    try{
+
+    }catch(error)
+    {
+        
+    }
+}
